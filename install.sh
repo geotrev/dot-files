fa90d3bcc29677ed5956544e0bb63f4f447d4755
George Treviranus <3946669+geotrev@users.noreply.github.com>
2023-05-25 21:30:39 -0500

    chore: edit install

--- install.sh
+++ install.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 
-create_symlinks() {
-    # Get the directory in which this script lives.
-    script_dir=$(dirname "$(readlink -f "$0")")
+# create_symlinks() {
+#     # Get the directory in which this script lives.
+#     script_dir=$(dirname "$(readlink -f "$0")")
 
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
+#     # Get a list of all files in this directory that start with a dot.
+#     files=$(find -maxdepth 1 -type f -name ".*")
 
-    # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
-}
+#     # Create a symbolic link to each file in the home directory.
+#     for file in $files; do
+#         name=$(basename $file)
+#         echo "Creating symlink to $name in home directory."
+#         rm -rf ~/$name
+#         ln -s $script_dir/$name ~/$name
+#     done
+# }
 
-create_symlinks
+# create_symlinks
 
-# Install Fira Code
+# # Install Fira Code
 
-dl_and_install_fira() {
-  fonts_dir="${HOME}/.local/share/fonts"
-  if [ ! -d "${fonts_dir}" ]; then
-      echo "mkdir -p $fonts_dir"
-      mkdir -p "${fonts_dir}"
-  else
-      echo "Found fonts dir $fonts_dir"
-  fi
+# dl_and_install_fira() {
+#   fonts_dir="${HOME}/.local/share/fonts"
+#   if [ ! -d "${fonts_dir}" ]; then
+#       echo "mkdir -p $fonts_dir"
+#       mkdir -p "${fonts_dir}"
+#   else
+#       echo "Found fonts dir $fonts_dir"
+#   fi
 
-  version=5.2
-  zip=Fira_Code_v${version}.zip
-  curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
-  unzip -o -q -d ${fonts_dir} ${zip}
-  rm ${zip}
+#   version=5.2
+#   zip=Fira_Code_v${version}.zip
+#   curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
+#   unzip -o -q -d ${fonts_dir} ${zip}
+#   rm ${zip}
 
-  echo "fc-cache -f"
-  fc-cache -f
-}
+#   echo "fc-cache -f"
+#   fc-cache -f
+# }
 
-# Use Soliah Zsh Theme
+# # Use Soliah Zsh Theme
 
-ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-mv -f Soliah.zsh-theme $ZSH_CUSTOM/themes/Soliah.zsh-theme
+# ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+# mv -f Soliah.zsh-theme $ZSH_CUSTOM/themes/Soliah.zsh-theme
 
-create_symlinks
-dl_and_install_fira
+# create_symlinks
+# dl_and_install_fira
 zsh
