3cfcf49ec995b514a9c83b843e9fcc67fed5eb28
george treviranus <geowtrev@gmail.com>
2023-05-26 17:45:14 -0500

    chore: update script

--- install.sh
+++ install.sh
@@ -19,19 +19,32 @@ function install_fira() {
 
   echo "fc-cache -f"
   fc-cache -f
-  
+
   echo "Fira Code install"
 }
 
+function install_theme() {
+  echo "Installing Soliah theme..."
+  omz_dir="${HOME}/.oh-my-zsh"
+
+  if [ ! -d "${omz_dir}" ]; then
+    mv -f .soliah.zsh-theme $omz_dir/themes/Soliah.zsh-theme
+    echo "Soliah theme created"
+  else
+    echo "OMZ directory doesn't exist, skipping"
+  fi
+}
+
+function install_zshrc() {
+  echo "Installing autosuggestions..."                
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  echo "Autosuggestions installed"
+
+  echo "Moving zshrc to root dir..."
+  cat .zshrc > $HOME/.zshrc
+  echo "zshrc moved"
+}
+
 install_fira
-
-# move custom Soliah theme 
-echo "Installing Soliah theme..."
-OMZ_DIR="${HOME}/.oh-my-zsh"
-mv -f Soliah.zsh-theme $OMZ_DIR/themes/Soliah.zsh-theme
-echo "Soliah theme created"
-
-echo "Moving zshrc to root dir..."
-# Move zshrc to codespaces home
-cat .zshrc > $HOME/.zshrc
-echo "zshrc moved"
+install_theme
+install_zshrc
